0c08f5ac462d9bb12c21f88cbc8abde93504dc51
Wenzheng Zeng <wenzhengz@parcelsanta.com>
2022-02-26 00:07:40 +0800

    测试以前设置

--- app/src/main/java/cn/kevin/openeye/logic/repository/DiscoveryPagingSource.kt
+++ app/src/main/java/cn/kevin/openeye/logic/repository/DiscoveryPagingSource.kt
@@ -10,26 +10,24 @@ import com.google.gson.Gson
 class DiscoveryPagingSource(private val homePageService: HomePageService) : PagingSource<String, Discovery.Item>() {
 
     override suspend fun load(params: LoadParams<String>): LoadResult<String, Discovery.Item> {
-       /* return try {
+        return try {
             val page = params.key ?:  HomePageService.DISCOVERY_URL
-            val pageSize = params.loadSize
+            //val pageSize = params.loadSize
             val repoResponse = homePageService.getDiscovery(page)
-            val test = Gson().toJson(repoResponse)
-            logD("zwz DiscoveryPagingSource:", test)
+            //val test = Gson().toJson(repoResponse)
+            //logD("zwz DiscoveryPagingSource:", test)
             val repoItems = repoResponse.itemList
             //val prevKey = if (page > 1) page - 1 else null
             val prevKey = null
             val nextKey = if (repoItems.isNotEmpty() && !repoResponse.nextPageUrl.isNullOrEmpty()) repoResponse.nextPageUrl else null
-            val ret = LoadResult.Page(repoItems, prevKey, nextKey)
-            logD("zwz", Gson().toJson(ret))
-            return ret
+            return LoadResult.Page(repoItems, prevKey, nextKey)
         } catch (e: Exception) {
             e.printStackTrace()
             LoadResult.Error(e)
-        }*/
+        }
 
 
-        return try {
+       /* return try {
             val page = params.key ?: HomePageService.DISCOVERY_URL
             val repoResponse = homePageService.getDiscovery(page)
             val repoItems = repoResponse.itemList
@@ -39,7 +37,7 @@ class DiscoveryPagingSource(private val homePageService: HomePageService) : Pagi
         } catch (e: Exception) {
             e.printStackTrace()
             LoadResult.Error(e)
-        }
+        }*/
     }
 
     override fun getRefreshKey(state: PagingState<String, Discovery.Item>): String? = null
